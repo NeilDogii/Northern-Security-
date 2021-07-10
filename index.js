dc35298dcdf51cef507132f25890d9d42d17556a
NeilDogii <aryaworrior41@gmail.com>
2021-07-10 10:34:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const {Client, Discord} = require("discord.js")
+const client = new Client();
+require(functions)(client)
+client.login("")
+
+module.export = {
+    client: client,
+    Discord: Discord
+}
