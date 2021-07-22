7654f89071fc63cf8209be00aacbda2a234e7e1a
NeilDogii <aryaworrior41@gmail.com>
2021-07-22 15:49:32 +0000

    yes

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -3,11 +3,13 @@ module.exports = {
   
     code(client, message, args, prefix) {
       if(message.author.id != "430964083160776705") return;
-      require('child_process').exec(__dirname + "/restart.bat", function (err, stdout, stderr) {
+      require('child_process').exec("/root/bot/restart.bat", function (err, stdout, stderr) {
           if (err) {
               return console.log(err);
           }
           console.log(stdout);
+          console.log("Done!")
+          message.reply(stdout)
       });
     }
   }
