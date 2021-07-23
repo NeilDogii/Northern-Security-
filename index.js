abc7e04e30e46dcc45606b5002913fb6c8ba37b6
NeilDogii <48602032+NeilDogii@users.noreply.github.com>
2021-07-23 19:41:20 +0530

    yes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const {Client, Discord, Intents} = require("discord.js")
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const config = require("./config.json")
-
 require("./functions")(client)
 
 exports.client = client;
-
 
 client.login(config.token)
