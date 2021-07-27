74c54420e6870f6744df640e4423a0b7c9e41c65
Vexcon1 <53382767+Vexcon1@users.noreply.github.com>
2021-07-27 14:09:15 -0400

    Update restart.js

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "restart", 
   
     code(client, message, args, prefix) {
-      if(message.author.id != "430964083160776705") return;
+      if(message.author.id != "430964083160776705" && message.author.id != "539395263257903104" && message.author.id != "424639704122654731") return;
       require('child_process').exec("/root/bot/restart.bat", function (err, stdout, stderr) {
           if (err) {
               return console.log(err);
