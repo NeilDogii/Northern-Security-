31a8f212c4d1789ec3d6b3829498f74d3c0e076b
Vexcon1 <53382767+Vexcon1@users.noreply.github.com>
2021-07-27 14:09:03 -0400

    Update pull.js

--- src/commands/pull.js
+++ src/commands/pull.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "pull", 
 
   code(client, message, args, prefix) {
-      if(message.author.id != "430964083160776705") return;
+      if(message.author.id != "430964083160776705" && message.author.id != "539395263257903104" && message.author.id != "424639704122654731") return;
     require('child_process').exec("/root/bot/pull.bat", function (err, stdout, stderr) {
         if (err) {
           message.member.send(err)
