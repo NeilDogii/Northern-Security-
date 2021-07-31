ba91db9c0d994497ba002d4dac95648de24ba0ec
NeilDogii <aryaworrior41@gmail.com>
2021-07-31 18:58:50 +0000

    yes sir

--- src/commands/addgb.js
+++ src/commands/addgb.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "addgb",
 
-  code(client, message, args, prefix, gdb) {
+  code(client, message, args, prefix, gbd) {
     if (message.author.id !== "302441957626609664" || message.author.id !== "539395263257903104") return message.reply("You Don't Have Permission")
     
 //     if (!args[0]) return message.reply("Missing UserId")
