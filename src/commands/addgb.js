4c7ad5fa9416a8dd7960327cf8373da7c8a1235b
Fern <915642604cc@gmail.com>
2021-07-31 21:59:26 +0800

    ok

--- src/commands/addgb.js
+++ src/commands/addgb.js
@@ -1,24 +1,23 @@
-const mongoose = require('mongoose');
-const Discord = require("discord.js")
-const gbd = mongoose.model("globalban", mongoose.Schema({
-  userid: Number,
-  whobanned: String
-}));
+// const mongoose = require('mongoose');
+// const Discord = require("discord.js")
+// const gbd = mongoose.model("globalban", mongoose.Schema({
+//   userid: Number,
+//   whobanned: String
+// }));
 
-module.exports = {
-  name: "addgb",
+// module.exports = {
+//   name: "addgb",
  
-
-  code(client, message, args, prefix) {
-    if (message.author.id !== "302441957626609664" || message.author.id !== "539395263257903104") return message.reply("You Don't Have Permission")
+//   code(client, message, args, prefix) {
+//     if (message.author.id !== "302441957626609664" || message.author.id !== "539395263257903104") return message.reply("You Don't Have Permission")
     
-    if (!args[0]) return message.reply("Missing UserId")
+//     if (!args[0]) return message.reply("Missing UserId")
 
-    let newd = new gbd({
-      userid = args[0],
-      whobanned = message.author.tag
-    })
-    newd.save().catch(err => console.log(err));
-    message.reply("Done")
-  }
-}
+//     let newd = new gbd({
+//       userid = args[0],
+//       whobanned = message.author.tag
+//     })
+//     newd.save().catch(err => console.log(err));
+//     message.reply("Done")
+//   }
+// }
